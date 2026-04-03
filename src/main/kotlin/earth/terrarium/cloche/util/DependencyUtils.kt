bcb1d0bb839ad9a82af7f955a30387a1e2717a2e
SettingDust <settingdust@gmail.com>
2026-04-03 23:03:19 +0800

    feat: add helper function to create target dependency

--- src/main/kotlin/earth/terrarium/cloche/util/DependencyUtils.kt
+++ src/main/kotlin/earth/terrarium/cloche/util/DependencyUtils.kt
@@ -1,7 +1,14 @@
 package earth.terrarium.cloche.util
 
+import earth.terrarium.cloche.ClocheExtension
+import earth.terrarium.cloche.ClocheTargetAttribute
+import earth.terrarium.cloche.api.target.ClocheTarget
 import org.gradle.api.artifacts.ExternalModuleDependency
 import org.gradle.api.artifacts.dsl.DependencyFactory
+import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.project
+import org.gradle.kotlin.dsl.the
 
 fun DependencyFactory.createLoaderDependency(
     group: String,
@@ -17,3 +24,16 @@ fun DependencyFactory.createLoaderDependency(
         configure()
     }
 }
+
+@Suppress("UnstableApiUsage")
+fun DependencyHandler.target(target: ClocheTarget) = project(":").apply {
+    capabilities {
+        requireFeature(target.capabilitySuffix!!)
+    }
+
+    attributes {
+        attribute(ClocheTargetAttribute.ATTRIBUTE, target.name)
+    }
+}
+
+fun DependencyHandler.target(name: String) = target(the<ClocheExtension>().targets[name])
